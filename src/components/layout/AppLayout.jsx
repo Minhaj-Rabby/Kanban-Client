f26e0772dd5fc09c1fa998732c343f1bfc7fa4c6
Minhaj-Rabby <minhaj12113@gmail.com>
2023-08-07 01:44:01 +0600

    Config Axios

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import authUtils from '../../utils/authUtils'
+import { Box} from '@mui/material'
+import Loading from '../common/Loading'
+import Sidebar from '../common/Sidebar'
 
 const AppLayout = () => {
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await authUtils.isAuthenticated()
+      if (!user) {
+
+        navigate('/Login')
+      }
+      else {
+        //save User
+        setLoading(false)
+
+      }
+
+    }
+    checkAuth()
+  }, [navigate])
   return (
-    <div>
-      <Outlet/>
-    </div>
+    loading ? (
+      <Loading fullheight></Loading>
+    ) : (
+
+      <Box sx={{
+
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}>
+        <Sidebar>
+          <Box sx={{
+            flexGrow:1,
+            p:1,
+            width:'max-content'
+          }}>
+            <Outlet></Outlet>
+
+          </Box>
+        </Sidebar>
+
+      </Box>
+
+    )
   )
 }
 
--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import authUtils from '../../utils/authUtils'
+import Loading from '../common/Loading'
+import { Box, Container } from '@mui/material'
+import assests from '../../assets'
 
 const AuthLayout = () => {
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const checkAuth = async () => {
+      const isAuth = await authUtils.isAuthenticated()
+      if (!isAuth) {
+        setLoading(false)
+      }
+      else {
+        navigate('/')
+      }
+
+    }
+    checkAuth()
+  }, [navigate])
+
   return (
-    <div>
-      <Outlet>
-        
-      </Outlet>
-    </div>
+    loading ? (
+      <Loading fullheight></Loading>
+    ) : (
+      <Container component='main' maxWidth='xs'>
+        <Box sx={{
+          marginTop: 8,
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column'
+        }}>
+          <img src={assests.images.logoDark} style={{ width: '100px' }} alt='app logo' />
+          <Outlet />
+        </Box>
+
+      </Container>
+    )
   )
 }
 
