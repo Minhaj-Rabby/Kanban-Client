8e041ae157ed960b910b6b8234c52b31150cd835
Minhaj-Rabby <minhaj12113@gmail.com>
2023-08-08 01:56:23 +0600

    Sidebar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import LoadingButton from '@mui/lab/LoadingButton'
+import { useDispatch } from "react-redux"
+//import { setBoards } from "../redux/features/boardSlice"
+import { useNavigate } from "react-router-dom"
+//import boardApi from "../api/boardApi"
+import { useState } from "react"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box sx={{
+      height: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <LoadingButton
+        variant='outlined'
+        color='success'
+        onClick={createBoard}
+        //loading={loading}
+      >
+        Click here to create your first board
+      </LoadingButton>
+    </Box>
   )
 }
 
